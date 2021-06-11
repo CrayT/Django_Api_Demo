7f098ff4c63ba13238a76d16febd1ef846d996f5
徐涛 <xutao@123kanfang.com>
2021-06-11 14:58:07 +0800

    update

--- api_demo/views.py
+++ api_demo/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework import permissions
 from django.http import HttpResponse,JsonResponse
 
 @api_view(['POST'])
-def post_demo(request, name):
-    print('request', request)
+def post_demo(request):
+    print('请求参数\n', request.query_params)
     return JsonResponse({
         'result':'success',
-        'method':'post'
+        'method':'Post'
     })
+
 @api_view(['GET'])
-def get_demo(request, name):
-    print('request',request,'\nname:', name)
+# @permission_classes((permissions.AllowAny,))
+def get_demo(request):
+    print('请求参数\n',request.query_params,'\nname:')
     return JsonResponse({
         'result':'success',
-        'method':'get'
+        'method':'Get'
     })
